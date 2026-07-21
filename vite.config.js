d27af6aeed8fa8702aa60c11e6ed3f3703fe03f8
pateldeep04 <pateldeep042007@gmail.com>
2026-07-21 18:00:37 +0530

    fix: add proxy error handlers to vite.config.js to suppress ECONNREFUSED startup log noise

--- vite.config.js
+++ vite.config.js
@@ -12,12 +12,25 @@ export default defineConfig({
         target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
+        configure: (proxy) => {
+          proxy.on('error', (err, req, res) => {
+            if (err.code === 'ECONNREFUSED' && res && !res.headersSent) {
+              res.writeHead(503, { 'Content-Type': 'application/json' });
+              res.end(JSON.stringify({ error: 'Backend server is starting up...' }));
+            }
+          });
+        }
       },
       '/socket.io': {
         target: 'http://127.0.0.1:5000',
         ws: true,
         changeOrigin: true,
         secure: false,
+        configure: (proxy) => {
+          proxy.on('error', (err) => {
+            // Silently handle startup WebSocket proxy connections
+          });
+        }
       }
     }
   }
